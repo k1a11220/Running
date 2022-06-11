d9c4b149db254fecc625e99401a5b4259feb14be
Son Beomsoo <k1a11220@naver.com>
2022-06-11 13:45:01 +0900

    Clean up

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import Maps from "../components/maps";
-
-const Home: NextPage = () => {
-  return <Maps latitude={37.5642135} longitude={127.0016985} />;
-};
-
-export default Home;
